1e6f81fbb68ce7f2754e970825ff282f69504323
Bazarsad Uurdmandakh <ubazraa3@gmail.com>
2025-06-01 00:01:41 +0900

    feat(icon): changed

--- src/layouts/header.layout.tsx
+++ src/layouts/header.layout.tsx
@@ -1,13 +1,13 @@
-import { MAXW } from '@/utils/globals';
-import { Box, Center, HStack, Image, Text } from '@chakra-ui/react';
-import Link from 'next/link';
-import { orbitron } from '@/fonts';
-import { useAccount, useSwitchChain } from 'wagmi';
-import Hamburger from './sideBar';
-import { useEffect, useState } from 'react';
-import { apeChain } from 'viem/chains';
-import WalletConnector from '@/utils/connectKit.custom';
-import { AccountDetails } from './accountDetails';
+import { MAXW } from "@/utils/globals";
+import { Box, Center, HStack, Image, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import { orbitron } from "@/fonts";
+import { useAccount, useSwitchChain } from "wagmi";
+import Hamburger from "./sideBar";
+import { useEffect, useState } from "react";
+import { apeChain } from "viem/chains";
+import WalletConnector from "@/utils/connectKit.custom";
+import { AccountDetails } from "./accountDetails";
 
 const Header = () => {
   const { chain, address } = useAccount();
@@ -28,9 +28,9 @@ const Header = () => {
       setIsScrolled(window.scrollY > 50); // Change threshold as needed
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -41,72 +41,72 @@ const Header = () => {
   }, [address, chain]);
   return (
     <HStack
-      pos="sticky"
-      top="0"
+      pos='sticky'
+      top='0'
       zIndex={4}
-      w="100%"
-      justifyContent="center"
-      py={['40px']}
-      px={'20px'}
+      w='100%'
+      justifyContent='center'
+      py={["40px"]}
+      px={"20px"}
 
       // bg={isScrolled ? "rgba(0,0,0,.2)" : "transparent"}
     >
       <HStack
-        w="100%"
-        justifyContent="space-between"
-        color="white"
+        w='100%'
+        justifyContent='space-between'
+        color='white'
         maxW={MAXW}
         fontWeight={700}
-        bg="black"
-        py="10px"
-        px="19px"
-        borderRadius="10px"
+        bg='black'
+        py='10px'
+        px='19px'
+        borderRadius='10px'
         border={
           isScrolled
-            ? '1px solid rgb(112, 112, 112)'
-            : '1px solid rgb(23, 25, 27)'
+            ? "1px solid rgb(112, 112, 112)"
+            : "1px solid rgb(23, 25, 27)"
         }
-        transition="ease-out 0.2s"
+        transition='ease-out 0.2s'
       >
         {/* <HStack spacing="48px">
         <Hamburger />
         </HStack> */}
         <HStack>
-          <Link href="https://hii.link">
-            <HStack pos="relative" alignItems="flex-end">
-              <Image src="/hiilink_header.avif" h="25px" />
+          <Link href='https://hii.link'>
+            <HStack pos='relative' alignItems='flex-end'>
+              <Image src='/hiilink.png' h='25px' />
             </HStack>
           </Link>
         </HStack>
 
         <HStack
-          spacing="24px"
-          alignItems="center"
-          pos="absolute"
-          left="50%"
-          transform="translateX(-50%)"
-          display={['none', null, 'flex', null]}
+          spacing='24px'
+          alignItems='center'
+          pos='absolute'
+          left='50%'
+          transform='translateX(-50%)'
+          display={["none", null, "flex", null]}
         >
-          <Link href="/">
+          <Link href='/'>
             <Text
               fontWeight={400}
-              fontSize="16px"
+              fontSize='16px'
               className={`${orbitron.className} header_gradient`}
             >
               Ongoing
             </Text>
           </Link>
-          <Link href="/history">
+          <Link href='/history'>
             <Text
               fontWeight={400}
-              fontSize="16px"
+              fontSize='16px'
               className={`${orbitron.className} header_gradient`}
             >
               History
             </Text>
           </Link>
         </HStack>
-        <HStack display={['flex', 'flex', 'flex', null]}>
+        <HStack display={["flex", "flex", "flex", null]}>
           {address ? <AccountDetails /> : <WalletConnector />}
         </HStack>
       </HStack>
